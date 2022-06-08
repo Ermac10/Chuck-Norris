920296c2620800c8ada407362ea8a3bbe67e9739
garciaej10 <garciacuff@gmail.com>
2022-06-07 22:32:49 -0600

    Can retrieve value (joke) from API and display as H1

--- script.js
+++ script.js
@@ -1,13 +1,31 @@
 // function getJoke (event) {    
 //     event.preventDefault()
 //     let url = "https://api.chucknorris.io/jokes/random"
- const url = "https://api.chucknorris.io/jokes/random"
-    ///FETCH code
+//  const url = "https://api.chucknorris.io/jokes/random"
+//     ///FETCH code
+//     fetch(url)
+//     .then(res => {
+//         return res.json();
+//     })
+//         .then(data => console.log(data))
+//         .catch(err => {
+//           console.log("something went wrong...", err);
+//         });
+
+
+function getJoke (event) {
+    // event.preventDefault()
+    let url = "https://api.chucknorris.io/jokes/random"
     fetch(url)
+    .then(res => res.json())
     .then(res => {
-        return res.json();
+        document.querySelector('h1').innerHTML = res.value
     })
-        .then(data => console.log(data))
         .catch(err => {
           console.log("something went wrong...", err);
-        });
+        });    
+}
+
+getJoke()
+        
+           
